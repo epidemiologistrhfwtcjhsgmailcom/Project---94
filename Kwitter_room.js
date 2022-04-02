b7a10d1c7482b4ab3da89c9d999d6d50fe1c8e49
epidemiologistrhfwtcjhsgmailcom <93857935+epidemiologistrhfwtcjhsgmailcom@users.noreply.github.com>
2022-04-02 10:30:35 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDbasrULxGTeeMANjPcCVYtrGojX3C5EN0",
+    authDomain: "project---94-82faf.firebaseapp.com",
+    projectId: "project---94-82faf",
+    storageBucket: "project---94-82faf.appspot.com",
+    messagingSenderId: "391312746140",
+    appId: "1:391312746140:web:d12685dbd94d2bbbeeaf59"
+  };
+
+firebase.initializeApp(firebaseConfig);
